feed9800dd47cccee13007e7974694135275c390
YuridiaGG <2330072@upt.edu.mx>
2026-05-31 20:06:51 -0600

    Se agrega Login, navegación entre vistas y mejoras del proyecto

--- app/shared/src/commonMain/kotlin/com/example/rutaupt/screens/HomeEstudianteScreen.kt
+++ app/shared/src/commonMain/kotlin/com/example/rutaupt/screens/HomeEstudianteScreen.kt
@@ -1,9 +1,18 @@
 package com.example.rutaupt.screens
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun HomeEstudianteScreen() {
-    Text("Bienvenido a RutaUPT")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Bienvenido Estudiante a RutaUPT")
+    }
 }
--- app/shared/src/commonMain/kotlin/com/example/rutaupt/storage/LocalSettings.kt
+++ app/shared/src/commonMain/kotlin/com/example/rutaupt/storage/LocalSettings.kt
@@ -2,27 +2,17 @@ package com.example.rutaupt.storage
 
 class LocalSettings {
 
-    // Simulación de un almacenamiento en memoria (SharedPreferences local)
     private val localStorage = mutableMapOf<String, String>()
 
-    /**
-     * Guarda el ID del rol (Estudiante, Chofer, Admin) localmente
-     */
     fun guardarRolUsuario(idRol: Int) {
         localStorage["id_rol"] = idRol.toString()
     }
 
-    /**
-     * Obtiene el rol guardado para saber qué pantalla mostrar al abrir la app
-     */
     fun obtenerRolUsuario(): Int {
         val rol = localStorage["id_rol"] ?: "0"
         return rol.toInt()
     }
 
-    /**
-     * Borra los datos locales cuando el usuario cierra sesión
-     */
     fun limpiarSesion() {
         localStorage.clear()
     }
