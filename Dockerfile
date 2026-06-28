50e76c711cfaf3045a0193078e260f35627db7be
YuridiaGG <2330072@upt.edu.mx>
2026-06-28 14:10:36 -0600

    Fix server build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 # Fase de compilación
 FROM gradle:8.7-jdk21 AS build
 
+# Fase de compilación
+FROM gradle:8.5-jdk17 AS build
+
 COPY --chown=gradle:gradle . /home/gradle/src
+
 WORKDIR /home/gradle/src
 
-# Dar permisos al wrapper
 RUN chmod +x gradlew
-
 RUN ./gradlew :server:build -x test --no-daemon
 
 # Fase ejecución
@@ -16,6 +18,6 @@ EXPOSE 8080
 
 RUN mkdir /app
 
-COPY --from=build /home/gradle/src/server/build/libs/*.jar /app/app.jar
+COPY --from=build /home/gradle/src/server/build/libs/*.jar /app/ktor-server.jar
 
-ENTRYPOINT ["java","-jar","/app/app.jar"]
+ENTRYPOINT ["java","-jar","/app/ktor-server.jar"]
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,27 +1,25 @@
 plugins {
     alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.ktor)
     alias(libs.plugins.serialization)
+    application
 }
 
 group = "com.example.rutaupt"
 version = "1.0.0"
 
 application {
-    mainClass = "com.example.rutaupt.ApplicationKt"
+    mainClass.set("com.example.rutaupt.ApplicationKt")
 }
 
 dependencies {
     api(projects.core)
     implementation(libs.logback)
-    
-    // Ktor Server
+
     implementation(libs.ktor.serverCore)
     implementation(libs.ktor.serverNetty)
-    implementation(libs.ktor.server.content.negotiation) // Corregido a SERVER
+    implementation(libs.ktor.server.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
-    
-    // Database (MySQL)
+
     implementation(libs.exposed.core)
     implementation(libs.exposed.dao)
     implementation(libs.exposed.jdbc)
